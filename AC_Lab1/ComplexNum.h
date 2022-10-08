b84b2eedd5b713cc32fa3f0e4fcf1bc81e58b7ca
Dm <Eton2017@icloud.com>
2022-10-08 10:56:00 +0300

    move files to the new folder

---
+++ AC_Lab1/ComplexNum.h
@@ -0,0 +1,58 @@
+#pragma once
+#include <iostream>
+class ComplexNum
+{
+private:
+	double real;
+	double imag;
+public:
+	ComplexNum(double real = 0.0, double imag = 0.0) {
+		this->real = real;
+		this->imag = imag;
+	}
+
+	//~ComplexNum(){}
+
+	void operator=(const ComplexNum& c1) {
+		/*ComplexNum res(c1.real, c1.imag);
+		return res;*/
+		this->real = c1.real;
+		this->imag = c1.imag;
+	}
+
+	ComplexNum operator+(const ComplexNum& c1)const {
+		ComplexNum new_num;
+		new_num.real = this->real + c1.real;
+		new_num.imag = this->imag + c1.imag;
+		return new_num;
+	}
+
+	ComplexNum operator-(const ComplexNum& c1)const {
+		ComplexNum new_num;
+		new_num.real = this->real - c1.real;
+		new_num.imag = this->imag - c1.imag;
+		return new_num;
+	}
+
+	ComplexNum operator*(const ComplexNum& c1)const {
+		ComplexNum new_num;
+		new_num.real = this->real * c1.real - this->imag * c1.imag;
+		new_num.imag = this->imag * c1.real + this->real * c1.imag;
+		return new_num;
+	}
+
+	friend std::ostream& operator<<(std::ostream& os, const ComplexNum& c) {
+		if (c.real) {
+			os << c.real;
+		}
+		if (c.imag >= 0) {
+			os << "+" << c.imag << "i";
+		}
+		else {
+			os << c.imag << "i";
+		}
+		return os;
+	}
+
+};
+
