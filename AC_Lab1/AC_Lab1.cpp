97daf702e5b5d6f43e7d0a5aae4b7b67d3d0f22b
Dm <Eton2017@icloud.com>
2022-10-08 12:40:37 +0300

    feat: added division operator

--- AC_Lab1/AC_Lab1.cpp
+++ AC_Lab1/AC_Lab1.cpp
@@ -3,6 +3,7 @@
 #include "ComplexNum.h"
 #include "ComplexMatrix.h"
 #include "Strassen.h"
+#include "LU_Inverse.h"
 #include <time.h>
 
 int main()
@@ -23,27 +24,30 @@ int main()
 	num = num1;
 	std::cout << num;
 	*/
+	ComplexNum c1(4, 5);
+	ComplexNum c2(3, 2);
+	std::cout << c1 << "\n";
+	std::cout << c2 << "\n";
+	ComplexNum c3 = c1 / c2;
+	std::cout << c3 << "\n\n_________________________________________________________________\n\n";
 
-	ComplexMatrix matr(10, 10);
-	matr.auto_gen(-2, 2, 0, 0);
-	/*matr.set(0, 0, 1, 2);
-	matr.set(0, 1, 0, -3);
-	matr.set(1, 0, 8, -2);
-	matr.set(1, 1, 0, 0);*/
-	matr.print();
-	std::cout << "A" << std::endl;
-	ComplexMatrix matr1(10, 10);
-	matr1.auto_gen(-4, 4, 0, 0);
-	/*matr1.set(0, 0, 2, 2);
-	matr1.set(0, 1, 4, -5);
-	matr1.set(1, 0, 1, -1);
-	matr1.set(1, 1, 5, 0);*/
-	matr1.print();
-	std::cout << "B" << std::endl;
-	ComplexMatrix* m = regular_mult(&matr, &matr1);
-	m->print();
-	std::cout << "C" << std::endl;
-	m = Strassen(&matr, &matr1);
-	m->print();
+
+	ComplexMatrix matr(2, 2);
+	matr.set(0, 0, 4, 0);
+	matr.set(0, 1, 3, 0);
+	matr.set(1, 0, 6, 0);
+	matr.set(1, 1, 3, 0);
+
+	ComplexMatrix matr1(13,4);
+	ComplexMatrix matr2(525, 1);
+
+	if (LU_decomposition(matr, matr1, matr2))
+	{
+		matr.print();
+		matr1.print();
+		matr2.print();
+	}
+
+	
 }
 
