0ab84913c902b42f5a98e34e3f1b040730553afe
Dm <Eton2017@icloud.com>
2022-10-10 22:03:08 +0300

    refactoring: changed tests format to iterate over possible matrix sizes + added srand

--- AC_Lab1/test.cpp
+++ AC_Lab1/test.cpp
@@ -26,11 +26,12 @@ TEST_CASE("Validity test: Strassen algorithm is equal to regular matrix multipli
 	CHECK(reg_res == *strassen_res);
 }
 
-TEST_CASE("speedtest for strassen algorithm") {
+TEST_CASE("speedtesting for the strassen algorithm") {
 	srand(time(0));
 	int start_size = 20;
 	int	end_size = 320;
 	std::clock_t start, end;
+	std::cout << "Speedtesting the Strassen algorithm";
 	for (int i = start_size; i <= end_size; i *= 2) {
 		ComplexMatrix m1(i, i);
 		ComplexMatrix m2(i, i);
@@ -39,19 +40,21 @@ TEST_CASE("speedtest for strassen algorithm") {
 		start = clock();
 		ComplexMatrix* res = Strassen(&m1, &m2);
 		end = clock();
-		std::cout << "\n N = " << i;
-		std::cout << "\n Start: " << start << " ticks";
-		std::cout << "\n End: " << end << " ticks";
-		std::cout << "\n Duration: " << end - start << " ticks";
+		std::cout << "\nN = " << i;
+		std::cout << "\nStart: " << start << " ticks";
+		std::cout << "\nEnd: " << end << " ticks";
+		std::cout << "\nDuration: " << end - start << " ticks";
 		bool checker = (*res == (m1 * m2));
 		delete res;
 		CHECK(checker);
 		std::cout << "\n";
 	}
+	std::cout << "\n";
 }
 
 TEST_CASE("testing the LU decomposition")
 {
+	srand(time(0));
 	int matrix_size = 4;
 	int tests_amount = 100;
 	ComplexMatrix A(matrix_size, matrix_size);
@@ -69,6 +72,7 @@ TEST_CASE("testing the LU decomposition")
 
 TEST_CASE("testing the inverse matrix by LU decomposition")
 {
+	srand(time(0));
 	int matrix_size = 4;
 	int tests_amount = 100;
 	ComplexMatrix A(matrix_size, matrix_size);
@@ -90,42 +94,50 @@ TEST_CASE("testing the inverse matrix by LU decomposition")
 
 TEST_CASE("speedtesting the LU decomposition")
 {
-	int matrix_size = 50;
-	int tests_amount = 20;
-	ComplexMatrix A(matrix_size, matrix_size);
-	ComplexMatrix L(matrix_size, matrix_size);
-	ComplexMatrix U(matrix_size, matrix_size);
+	srand(time(0));
+	int start_size = 20;
+	int	end_size = 320;
 	std::clock_t start, end;
 
-	for (int n = 0; n < tests_amount; n++)
-	{
-		A.auto_gen(2, 50, 2, 50);
+	std::cout << "Speedtesting the LU decomposition\n";
+
+	for (int i = start_size; i <= end_size; i *= 2) {
+		ComplexMatrix A(i, i);
+		ComplexMatrix L(i, i);
+		ComplexMatrix U(i, i);
+		A.auto_gen(1, 5, 1, 5);
 		bool res = true;
 		start = clock();
 		if (LU_decomposition(A, L, U))
 			res = (A == L * U);
 		end = clock();
-		std::cout << "The end: " << end << " ticks" << "\n";
-		std::cout << "The start: " << start << " ticks" << "\n";
-		std::cout << "Elapsed time: " << end - start << " ticks" << "\n\n";
+		std::cout << "\nN = " << i;
+		std::cout << "\nStart: " << start << " ticks";
+		std::cout << "\nEnd: " << end << " ticks";
+		std::cout << "\nDuration: " << end - start << " ticks";
 		CHECK(res);
+		std::cout << "\n";
 	}
+	std::cout << "\n";
 }
 
 TEST_CASE("speedtesting the inverse matrix by LU decomposition")
 {
-	int matrix_size = 200;
-	int tests_amount = 20;
-	ComplexMatrix A(matrix_size, matrix_size);
-	ComplexMatrix E(matrix_size, matrix_size);
+	srand(time(0));
+	int start_size = 20;
+	int	end_size = 320;
+
 	std::clock_t start, end;
 
-	for (int i = 0; i < matrix_size; i++)
-		E.set(i, i, ComplexNum(1));
 
-	for (int n = 0; n < tests_amount; n++)
-	{
-		A.auto_gen(2, 50, 6, 49);
+	std::cout << "Speedtesting the inverse matrix by LU decomposition\n\n";
+
+	for (int i = start_size; i <= end_size; i *= 2) {
+		ComplexMatrix A(i, i);
+		ComplexMatrix E(i, i);
+		for (int j = 0; j < i; j++)
+			E.set(j, j, ComplexNum(1));
+		A.auto_gen(1, 5, 1, 5);
 
 		start = clock();
 		ComplexMatrix Inverse_A = LU_inverse(A);
@@ -133,11 +145,15 @@ TEST_CASE("speedtesting the inverse matrix by LU decomposition")
 		bool res = true;
 		if (Inverse_A.get_rows() != 0)
 			res = (E == A * Inverse_A);
-		std::cout << "The end: " << end << " ticks" << "\n";
-		std::cout << "The start: " << start << " ticks" << "\n";
-		std::cout << "Elapsed time: " << end - start << " ticks" << "\n\n";
+
+		std::cout << "\nN = " << i;
+		std::cout << "\nStart: " << start << " ticks";
+		std::cout << "\nEnd: " << end << " ticks";
+		std::cout << "\nDuration: " << end - start << " ticks";
 		CHECK(res);
+		std::cout << "\n";
 	}
+	std::cout << "\n";
 }
 
 TEST_CASE("testing the equality of GaussJordan and LU inversions")
@@ -147,7 +163,7 @@ TEST_CASE("testing the equality of GaussJordan and LU inversions")
 		ComplexMatrix A(10, 10);
 		A.auto_gen(-10, 10, -10, 10);
 
-		A.print();
+		//A.print();
 
 		ComplexMatrix m1 = GaussJordanInverse(A);
 		ComplexMatrix m2 = LU_inverse(A);
